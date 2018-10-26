b24cc215fdf032e6bcc23f490818f21ef2cc616c
chrisamurao <freshpotatoes@gmail.com>
2018-10-26 00:19:19 -0700

    added score state

---
+++ flappy/flappy-9/states/TitleScreenState.lua
@@ -0,0 +1,22 @@
+--[[ Title Screen State Class
+
+The starting screen of the game.
+It should display "Press Enter and show the highest score."
+]]
+
+
+TitleScreenState = Class{__includes = BaseState}
+
+function TitleScreenState:update(dt)
+    if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+        gStateMachine:change('play')
+    end
+end
+
+function TitleScreenState:render()
+    love.graphics.setFont(flappyFont)
+    love.graphics.printf('Tippy Bird', 0, 64, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setFont(mediumFont)
+    love.graphics.printf('Press Enter', 0, 100, VIRTUAL_WIDTH, 'center')
+end
